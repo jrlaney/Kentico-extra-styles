7127c823ede8ccebaac38cc0da2a91a9ba35b32e
jrlaney <jlaney@microstrategy.com>
2017-05-10 18:20:40 -0400

    updates for new primary nav sample and html

--- z_sandbox/new-header/script sample backup.js
+++ z_sandbox/new-header/script sample backup.js
@@ -1,14 +1,19 @@
+<script type="text/javascript">
 jQuery(document).ready(function(){
+	//trigger sall menu open animations
 	$(".primary-menu-button").click(function() {
 	    $("body").toggleClass('menu-opened');
 	});
 
-	$("#primary-menu .section-link").click(function() {
-	    $("#primary-menu .section-link").removeClass('active');
+	//triggers for all the panels and sub panels in the menu
+	$("#primary-menu .parent-node").click(function() {
+	    $(this).siblings().removeClass('active');
 	    $(this).addClass('active');
 	});
+
+	if ($(".preview-info-text").length > 0) {
+	    $("body").addClass("CMS-preview-mode");
+	};
 });
 
-if ($(".PageManagerPreviewInfo").length > 0) {
-    $("body").addClass("CMS-preview-mode");
-}
+</script>
