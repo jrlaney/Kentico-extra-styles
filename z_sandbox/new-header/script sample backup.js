a5724a4044fca11a872c24b82d4537c74d73f549
jrlaney <jlaney@microstrategy.com>
2018-04-10 18:42:38 -0400

    committing last few new nav changes before testing

--- z_sandbox/new-header/script sample backup.js
+++ z_sandbox/new-header/script sample backup.js
@@ -1,22 +1,29 @@
-<script type="text/javascript">
 jQuery(document).ready(function(){
 	//trigger all menu open animations
 	$('.primary-menu-button').click(function() {
-	    $('body').toggleClass('menu-opened');
+	    $('body').toggleClass('primary-menu-opened');
 	});
 
 	//triggers for all the panels and sub panels in the menu
-	$('#primary-menu .parent-node > .sub-link').click(function() {
+	$('#primary-menu .parent-node > .menu-item').click(function() {
 	    $(this).parent('.parent-node').siblings().removeClass('active');
 		$(this).parent('.parent-node').siblings().find('.parent-node.active').removeClass('active');
 	    $(this).parent('.parent-node').addClass('active');
 	});
 
 	//triggers the back button traversal for mobile view
-	$('#primary-menu .parent-node .sub-link.back').click(function() {
+	$('#primary-menu .parent-node .menu-item.back').click(function() {
 		$(this).closest('.parent-node.active').removeClass('active');
 		$(this).siblings('.parent-node.active').removeClass('active');
 	});
+
+	//remove any initial active link in mobile view and clear active status when exiting the menu
+	if (screen.width <= 768) {
+		$('#primary-menu .parent-node').removeClass('active');
+		$('.primary-menu-button').click(function() {
+		    $('#primary-menu .parent-node').removeClass('active');
+		});
+	}
 
 	//set initial color based on active level-1
 	if ( $('#primary-menu .level-1 > .parent-node.active').hasClass('red')) {
@@ -36,7 +43,7 @@ jQuery(document).ready(function(){
 	}
 
 	//triggers color change for level-1 elements
-	$('#primary-menu .level-1 > .parent-node > .sub-link').click(function() {
+	$('#primary-menu .level-1 > .parent-node > .menu-item').click(function() {
 	    $('#primary-menu').removeClass();
 		if ( $(this).parent('.parent-node').hasClass('red')) {
 		    $('#primary-menu').addClass('red');
@@ -55,9 +62,8 @@ jQuery(document).ready(function(){
 		}
 	});
 
+	//adds a class to the body if the page is in preview mode so the fixed menu doesnt go under the text for that
 	if ($('.preview-info-text').length > 0) {
 	    $('body').addClass('CMS-preview-mode');
 	};
 });
-
-</script>
