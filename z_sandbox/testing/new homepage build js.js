42a682c7f05c17272dae1925438fe58236c5eee7
jrlaney <jlaney@microstrategy.com>
2019-07-23 17:44:33 -0400

    new homepage with macbook-pro hero

--- z_sandbox/testing/new homepage build js.js
+++ z_sandbox/testing/new homepage build js.js
@@ -1,36 +1,59 @@
 $(document).ready(function(){
-    //script that flips through the 3 diagram classe son a timer
+    var hovering = false;
+
+    //script that flips through the 3 diagram classes on a timer
     var images = ['hyperintelligence', 'mobility', 'federated-analytics'];
     var classIndex = -1;
     function changeBackground() {
-        var diagram = $("#top-benefits");
-        if (classIndex >= 0) {
-            diagram.removeClass(images[classIndex]);
+        if(!hovering) {
+            //console.log('class change ran');
+            var diagram = $('#top-benefits');
+            if (classIndex >= 0) {
+                diagram.attr('data-currentBenefit', '');
+            }
+            classIndex = (classIndex + 1) % images.length;
+            //diagram.addClass(images[classIndex]);
+            diagram.attr('data-currentBenefit', images[classIndex]);
         }
-        classIndex = (classIndex + 1) % images.length;
-        diagram.addClass(images[classIndex]);
-    }
+    };
     changeBackground();
     setInterval(changeBackground, 6000);
 
+    //$('#diagram-options').hover(hovering = true, hovering = false);
+    $("#diagram-options").hover(function() {
+        hovering = true
+        //console.log(hovering);
+    }, function() {
+        hovering = false
+        //console.log(hovering);
+    });
+
+
+    //change the animation on click
+    $('.benefit-col').click(function(){
+        var bene_id = $(this).attr('data-benefit');
+        $('#top-benefits').attr('data-currentBenefit', '');
+        $('#top-benefits').attr('data-currentBenefit', bene_id);
+    });
+
     //customer story slider script
     $('#customer-story-slider').slick({
-		autoplay: false,
-		//autoplaySpeed: 3000,
-		cssEase: 'ease-out',
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		draggable: true,
+        autoplay: false,
+        //autoplaySpeed: 3000,
+        cssEase: 'ease-out',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
         asNavFor: '#customer-story-nav',
         fade: true,
-		swipe: true,
-		touchMove: true,
-		pauseOnFocus: false,
-		pauseOnHover: false,
-		arrows: true,
-		prevArrow: '<button type="button" class="slick-prev"><i class="icon icon-chevron-left icon-block small black"></button>',
-		nextArrow: '<button type="button" class="slick-next"><i class="icon icon-chevron-right icon-block small black"></button>',
-	});
+        swipe: true,
+        touchMove: true,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="icon icon-chevron-left icon-block small black"></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="icon icon-chevron-right icon-block small black"></button>',
+    });
     $('#customer-story-nav').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
@@ -41,18 +64,18 @@ $(document).ready(function(){
         focusOnSelect: true,
         draggable: false,
         responsive: [
-    		{
-    			breakpoint: 980,
-    			settings: {
+            {
+                breakpoint: 980,
+                settings: {
                     slidesToShow: 3,
-    			}
-    		},
-    		{
-    			breakpoint: 768,
-    			settings: {
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
                     slidesToShow: 2,
-    			}
-    		}
-    	]
+                }
+            }
+        ]
     });
 });
